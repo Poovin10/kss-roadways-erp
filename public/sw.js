7a2b2ca4b4420962e493642256007472dff5ffad
Cyberghost <88696278+Poovin10@users.noreply.github.com>
2026-09-12 21:48:01 +0530

    sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,14 @@
 self.addEventListener('install', (e) => {
+  self.skipWaiting();
   console.log('[Service Worker] Installed');
 });
 
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+  console.log('[Service Worker] Activated');
+});
+
 self.addEventListener('fetch', (e) => {
-  // Chrome requires this fetch handler to allow the app to be installed
+  // This simple pass-through satisfies Chrome's PWA requirement perfectly
+  e.respondWith(fetch(e.request).catch(() => new Response("Network error")));
 });
