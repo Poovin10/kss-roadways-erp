7473df252a75dde792ed306e1a39797af3043b06
Cyberghost <88696278+Poovin10@users.noreply.github.com>
2026-09-13 21:59:46 +0530

    server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -6,7 +6,7 @@ export async function createClient() {
 
   return createServerClient(
     "https://eobweyciqwoojwnsonor.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImvYndleWNpcXdvb2p3bnNvbm9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MDAzNDMsImV4cCI6MjEwMzQ3NjM0M30.asNbhEvqmrGLNW7FA4Rys6XAHJSveN3B-13USG-INYI",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYndleWNpcXdvb2p3bnNvbm9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MDAzNDMsImV4cCI6MjEwMzQ3NjM0M30.asNbhEvqmrGLNW7FA4Rys6XAHJSveN3B-13USG-INYI",
     {
       cookies: {
         getAll() {
