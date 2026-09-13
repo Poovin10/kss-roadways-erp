3c1d28161e1dad482cf9366fe2fc3e11f6ac910a
Cyberghost <88696278+Poovin10@users.noreply.github.com>
2026-09-13 16:26:25 +0530

    client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,12 +1,8 @@
 import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-
-  if (!supabaseUrl || !supabaseKey) {
-    throw new Error("Your project's URL and Key are required to create a Supabase client!");
-  }
+  const supabaseUrl = "https://eobweyciqwoojwnsonor.supabase.co";
+  const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYndleWNpcXdvb2p3bnNvbm9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MDAzNDMsImV4cCI6MjEwMzQ3NjM0M30.asNbhEvqmrGLNW7FA4Rys6XAHJSveN3B-13USG-INYI";
 
   return createBrowserClient(supabaseUrl, supabaseKey);
 }
