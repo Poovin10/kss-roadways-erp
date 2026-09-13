476cf245f0f6544e74cf280627b446ad26fc74f0
Cyberghost <88696278+Poovin10@users.noreply.github.com>
2026-09-13 16:45:18 +0530

    proxy.ts

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -6,11 +6,9 @@ export async function proxy(request: NextRequest) {
     request,
   });
 
-  // With Fluid compute, don't put this client in a global environment
-  // variable. Always create a new one on each request.
   const supabase = createServerClient(
     "https://eobweyciqwoojwnsonor.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYndleWNpcXdvb2p3bnNvbm9yI","
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvYndleWNpcXdvb2p3bnNvbm9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MDAzNDMsImV4cCI6MjEwMzQ3NjM0M30.asNbhEvqmrGLNW7FA4Rys6XAHJSveN3B-13USG-INYI",
     {
       cookies: {
         getAll() {
@@ -31,12 +29,6 @@ export async function proxy(request: NextRequest) {
     },
   );
 
-  // Do not run code between createServerClient and
-  // supabase.auth.getClaims(). A simple mistake could make it very hard to debug
-  // issues with users being randomly logged out.
-
-  // IMPORTANT: If you remove getClaims() and you use server-side rendering
-  // with the Supabase client, your users may be randomly logged out.
   const { data } = await supabase.auth.getClaims();
   const user = data?.claims;
 
@@ -45,26 +37,12 @@ export async function proxy(request: NextRequest) {
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
-    !request.nextUrl.pathname.startsWith("/driver") // Whitelists the driver portal
+    !request.nextUrl.pathname.startsWith("/driver")
   ) {
-    // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
     url.pathname = "/auth/login";
     return NextResponse.redirect(url);
   }
-
-  // IMPORTANT: You *must* return the supabaseResponse object as it is.
-  // If you're creating a new response object with NextResponse.next() make sure to:
-  // 1. Pass the request in it, like so:
-  //    const myNewResponse = NextResponse.next({ request })
-  // 2. Copy over the cookies, like so:
-  //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
-  // 3. Change the myNewResponse object to fit your needs, but avoid changing
-  //    the cookies!
-  // 4. Finally:
-  //    return myNewResponse
-  // If this is not done, you may be causing the browser and server to go out
-  // of sync and terminate the user's session prematurely!
 
   return supabaseResponse;
 }
